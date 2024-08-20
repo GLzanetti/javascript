2119734b572f8905e39d98723320158699962995
Gabriel Zanetti <gabrielzanettigo@gmail.com>
2024-08-20 15:41:13 -0300

    aula sobre condicoes

---
+++ codigos/04/ex09.js
@@ -0,0 +1,7 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples
+    console.log(`Voce passou ascima do limite de velocidade.MULTADO!`)
+}
+console.log(`Dirija sempre com o sinto de segurança!`)
